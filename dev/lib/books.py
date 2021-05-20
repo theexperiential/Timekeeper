9fbba5a39f99c504346dd0020c90310fb34eddb6
rosco <rosco@theexperiential.com>
2021-05-19 23:10:16 -0700

    external json settings, books and timetable init

---
+++ dev/lib/books.py
@@ -0,0 +1,58 @@
+{
+	"Books": [
+		{
+			"Members": [
+				{
+					"name": "Anita",
+					"test": "a"
+				},
+				{
+					"name": "Dylan",
+					"test": "b"
+				}
+			]
+		},
+		{
+			"Customers": [
+				{
+					"name": "Weyland Industries"
+				},
+				{
+					"name": "ha"
+				},
+				{
+					"name": "Downstream"
+				}
+			]
+		},
+		{
+			"Projects": [
+				{
+					"name": "proj2"
+				},
+				{
+					"name": "proj3"
+				},
+				{
+					"name": "Weyland Industries"
+				},
+				{
+					"name": "TMO"
+				}
+			]
+		},
+		{
+			"Tasks": [
+				{
+					"name": "task2"
+				},
+				{
+					"name": "task5"
+				},
+				{
+					"name": "task4"
+				}
+			]
+		}
+	]
+}
---
+++ dev/lib/settings.py
@@ -0,0 +1,250 @@
+{
+	"General": {
+		"Companyname": {
+			"name": "Companyname",
+			"label": "Company Name",
+			"page": "General",
+			"style": "Str",
+			"default": "",
+			"enable": true,
+			"startSection": false,
+			"readOnly": false,
+			"enableExpr": null,
+			"val": "TEC"
+		},
+		"Sync": {
+			"name": "Sync",
+			"label": "Sync",
+			"page": "General",
+			"style": "Header",
+			"default": "",
+			"enable": true,
+			"startSection": true,
+			"readOnly": false,
+			"enableExpr": null,
+			"val": ""
+		},
+		"Booksfile": {
+			"name": "Booksfile",
+			"label": "Books File",
+			"page": "General",
+			"style": "File",
+			"default": "",
+			"enable": true,
+			"startSection": false,
+			"readOnly": false,
+			"enableExpr": null,
+			"val": "lib/books.py"
+		},
+		"Timesheetsfile": {
+			"name": "Timesheetsfile",
+			"label": "Timesheets File",
+			"page": "General",
+			"style": "File",
+			"default": "",
+			"enable": true,
+			"startSection": false,
+			"readOnly": false,
+			"enableExpr": null,
+			"val": "lib/timesheets.py"
+		}
+	},
+	"Appearance": {
+		"Theme": {
+			"name": "Theme",
+			"label": "Theme",
+			"page": "Appearance",
+			"style": "Menu",
+			"default": "Dark",
+			"enable": true,
+			"startSection": false,
+			"readOnly": false,
+			"enableExpr": null,
+			"menuNames": [
+				"Dark",
+				"Light"
+			],
+			"menuLabels": [
+				"Dark",
+				"Light"
+			],
+			"val": "Dark"
+		},
+		"Menubarbgcolor": {
+			"name": "Menubarbgcolor",
+			"label": "Menubar BG Color",
+			"page": "Appearance",
+			"style": "RGB",
+			"default": 0.0,
+			"enable": true,
+			"startSection": true,
+			"readOnly": false,
+			"enableExpr": null,
+			"min": 0.0,
+			"max": 1.0,
+			"normMin": 0.0,
+			"normMax": 1.0,
+			"clampMin": false,
+			"clampMax": false,
+			"val": 0.1
+		},
+		"Textcolor": {
+			"name": "Textcolor",
+			"label": "Text Color",
+			"page": "Appearance",
+			"style": "RGBA",
+			"default": 0.0,
+			"enable": true,
+			"startSection": false,
+			"readOnly": false,
+			"enableExpr": null,
+			"min": 0.0,
+			"max": 1.0,
+			"normMin": 0.0,
+			"normMax": 1.0,
+			"clampMin": false,
+			"clampMax": false,
+			"val": 0.0
+		},
+		"Accentcolor": {
+			"name": "Accentcolor",
+			"label": "Accent Color",
+			"page": "Appearance",
+			"style": "RGB",
+			"default": 0.0,
+			"enable": true,
+			"startSection": false,
+			"readOnly": false,
+			"enableExpr": null,
+			"min": 0.0,
+			"max": 1.0,
+			"normMin": 0.0,
+			"normMax": 1.0,
+			"clampMin": false,
+			"clampMax": false,
+			"val": [
+				0.8,
+				0.24,
+				0.0
+			]
+		},
+		"Restcolor": {
+			"name": "Restcolor",
+			"label": "Rest Color",
+			"page": "Appearance",
+			"style": "RGB",
+			"default": 0.0,
+			"enable": true,
+			"startSection": false,
+			"readOnly": false,
+			"enableExpr": null,
+			"min": 0.0,
+			"max": 1.0,
+			"normMin": 0.0,
+			"normMax": 1.0,
+			"clampMin": false,
+			"clampMax": false,
+			"val": [
+				0.0,
+				0.7999999523162842,
+				1.0
+			]
+		},
+		"Selectioncolor": {
+			"name": "Selectioncolor",
+			"label": "Selection Color",
+			"page": "Appearance",
+			"style": "RGBA",
+			"default": 0.0,
+			"enable": true,
+			"startSection": false,
+			"readOnly": false,
+			"enableExpr": null,
+			"min": 0.0,
+			"max": 1.0,
+			"normMin": 0.0,
+			"normMax": 1.0,
+			"clampMin": false,
+			"clampMax": false,
+			"val": [
+				0.7139999866485596,
+				0.7139999866485596,
+				0.7139999866485596,
+				1.0
+			]
+		},
+		"Winoffset": {
+			"name": "Winoffset",
+			"label": "Window Offset",
+			"page": "Appearance",
+			"style": "XY",
+			"default": 0.0,
+			"enable": true,
+			"startSection": false,
+			"readOnly": false,
+			"enableExpr": null,
+			"min": 0.0,
+			"max": 1.0,
+			"normMin": 0.0,
+			"normMax": 1.0,
+			"clampMin": false,
+			"clampMax": false,
+			"val": [
+				-153.0,
+				0.0
+			]
+		}
+	},
+	"Load": {
+		"Loadsettingsfile": {
+			"name": "Loadsettingsfile",
+			"label": "Load Settings File",
+			"page": "Load",
+			"style": "File",
+			"default": "",
+			"enable": true,
+			"startSection": false,
+			"readOnly": false,
+			"enableExpr": null,
+			"val": ""
+		}
+	},
+	"About": {
+		"Version": {
+			"name": "Version",
+			"label": "Version",
+			"page": "About",
+			"style": "Str",
+			"default": "",
+			"enable": true,
+			"startSection": false,
+			"readOnly": true,
+			"enableExpr": null,
+			"val": "0.1"
+		},
+		"Author": {
+			"name": "Author",
+			"label": "Author",
+			"page": "About",
+			"style": "Str",
+			"default": "",
+			"enable": true,
+			"startSection": false,
+			"readOnly": true,
+			"enableExpr": null,
+			"val": "Dylan Roscover"
+		},
+		"Contact": {
+			"name": "Contact",
+			"label": "Contact",
+			"page": "About",
+			"style": "Str",
+			"default": "",
+			"enable": true,
+			"startSection": false,
+			"readOnly": true,
+			"enableExpr": null,
+			"val": "rosco@tec.design"
+		}
+	}
+}
